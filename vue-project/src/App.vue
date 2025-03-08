4c27c03eb3d2c01ad05fc1bacb0d481407a5238f
Maria Theresa Carreon <122862523+theacarreon@users.noreply.github.com>
2025-03-08 23:50:40 +0800

    added components

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,7 +1,22 @@
 <script></script>
  
 <template>
-  <h1>Hello World!</h1>
+  <h1>Food</h1>
+  <food-item/>
+  <food-item/>
+  <food-item/>
 </template>
  
-<style></style>
+<style>
+  #app > div {
+    border: dashed black 1px;
+    display: inline-block;
+    margin: 10px;
+    padding: 10px;
+    background-color: lightgreen;
+  }
+
+  #app > div:hover {
+    cursor: pointer;
+  }
+</style>
