fd2c797a9bc1d45547f2505837331e201f5e3f84
Maria Theresa Carreon <122862523+theacarreon@users.noreply.github.com>
2025-03-09 00:48:08 +0800

    added personal profile

--- vue-project/src/components/PersonalProfile.vue
+++ vue-project/src/components/PersonalProfile.vue
@@ -1,9 +1,5 @@
 <template>
     <div id="about">
-    <div class="about-container">
-        <div class="profile-image">
-            <img src="images/thea.png" alt="Profile" class="profile-pic"> 
-        </div>
             <div class="info-box">
       <h2>"learn to live the unexamined life" - Fiyero, Wicked</h2>
       <p>Hi! my name is Maria Theresa B. Carreon. <br> But I prefer to be called as Thea.</p>
