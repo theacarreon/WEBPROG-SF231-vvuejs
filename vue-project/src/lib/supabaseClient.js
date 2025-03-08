f67d73e5b2149aa57256a5e81caf9e5d2456dc89
Maria Theresa Carreon <122862523+theacarreon@users.noreply.github.com>
2025-03-08 17:38:39 +0000

    Query Data from the app

--- vue-project/src/lib/supabaseClient.js
+++ vue-project/src/lib/supabaseClient.js
@@ -1,3 +1,3 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient{'https://kvmyaecyjxvgqobgykth.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt2bXlhZWN5anh2Z3FvYmd5a3RoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE0NTIzNzgsImV4cCI6MjA1NzAyODM3OH0.I8VhFN1uPbnF2RErGuWu8jsdgsU6QEET6-qcoDGxFsY'}
+export const supabase = createClient('https://kvmyaecyjxvgqobgykth.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt2bXlhZWN5anh2Z3FvYmd5a3RoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE0NTIzNzgsImV4cCI6MjA1NzAyODM3OH0.I8VhFN1uPbnF2RErGuWu8jsdgsU6QEET6-qcoDGxFsY')
