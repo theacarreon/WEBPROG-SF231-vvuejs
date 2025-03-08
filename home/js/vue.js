ae47bdba5cc723826eceb4eb800858a01e9a1d2c
Maria Theresa Carreon <122862523+theacarreon@users.noreply.github.com>
2025-03-08 18:38:09 +0800

    added html and css

--- home/js/vue.js
+++ home/js/vue.js
@@ -1,10 +1,10 @@
-
-  const app = Vue.createApp({
-   data() {
-    return {
-     message: "Hello World!"
+const app = Vue.createApp({
+    data() {
+      return {
+        message: "Hello World!"
+      };
     }
-   }
-  })
+  });
   
-  app.mount('#app')
+  app.mount('#app');
+  
