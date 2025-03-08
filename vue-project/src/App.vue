d1b4326753e4f14c47b7d2e88baea4429ef7c2fc
Maria Theresa Carreon <122862523+theacarreon@users.noreply.github.com>
2025-03-08 17:34:35 +0000

    Query Data from the app

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,6 +1,23 @@
-<script></script>
+<script setup>
+  import { ref, onMounted } from 'vue'
+  import { supabase } from './lib/supabaseClient'
+
+  const countries = ref([])
+
+  async function getCountries() {
+    const { data } = await supabase.from('countries').select()
+    instruments.value = data
+  }
+
+  onMounted(() => {
+    getCountries()
+  })
+  </script>
  
 <template>
+  <ul>
+    <li v-for="country in countries" :key="country.id">{{ country.name }}</li>
+    </ul>
   <h1>Food</h1>
   <PersonalProfile/>
   <food-item/>
